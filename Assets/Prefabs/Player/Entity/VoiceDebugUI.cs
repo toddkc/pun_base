7dd31c670676535c392be54b4d0d42bb95a82ddf
todd strix laptop <tcbkc111@gmail.com>
2020-07-11 17:45:15 -0500

    add debug for voice

---
+++ Assets/Prefabs/Player/Entity/VoiceDebugUI.cs
@@ -0,0 +1,21 @@
+﻿using Photon.Voice.PUN;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VoiceDebugUI : MonoBehaviour
+{
+    PhotonVoiceView view;
+    [SerializeField] Image micImage;
+    [SerializeField] Image speakerImage;
+
+    private void Start()
+    {
+        view = GetComponent<PhotonVoiceView>();
+    }
+
+    private void Update()
+    {
+        micImage.enabled = view.IsRecording;
+        speakerImage.enabled = view.IsSpeaking;
+    }
+}
