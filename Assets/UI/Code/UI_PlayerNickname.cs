b2c25e0da13f652ca8e791e665a8ea3eb92a661b
todd strix laptop <tcbkc111@gmail.com>
2020-07-11 11:44:26 -0500

    add a player nickname input to the main menu

---
+++ Assets/UI/Code/UI_PlayerNickname.cs
@@ -0,0 +1,34 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_PlayerNickname : MonoBehaviourPun
+{
+    [SerializeField] InputField nicknameInput = default;
+    private const string playerPrefsKey = "nickname";
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey(playerPrefsKey))
+        {
+            nicknameInput.text = PlayerPrefs.GetString(playerPrefsKey);
+        }
+        else
+        {
+            nicknameInput.text = "Player" + Random.Range(111111, 999999);
+        }
+    }
+
+    private void OnEnable()
+    {
+        nicknameInput.onValueChanged.AddListener(UpdatePlayerNickname);
+    }
+
+    private void UpdatePlayerNickname(string nickname)
+    {
+        //string nickname = nicknameInput.text;
+        PlayerPrefs.SetString(playerPrefsKey, nickname);
+        PhotonNetwork.NickName = nickname;
+        Debug.Log("player nickname set as: " + nickname);
+    }
+}
